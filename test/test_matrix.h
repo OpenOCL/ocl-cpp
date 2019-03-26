b1ba1b3f1f6bb079f0af6ed07da316eefb18aeb5
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-26 18:16:48 +0100

    tensor tests w 2dim vector, ocl_info

--- test/test_matrix.h
+++ test/test_matrix.h
@@ -21,99 +21,99 @@ TEST(Matrix, aScalarOperators)
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::uplus(a);
-    ocl::test::assertEqual( ocl::full(r), 4 );
+    ocl::test::assertEqual( ocl::full(r), 4, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::uminus(a);
-    ocl::test::assertEqual( ocl::full(r), -4 );
+    ocl::test::assertEqual( ocl::full(r), -4, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::exp(a);
-    ocl::test::assertEqual( ocl::full(r), 54.5981500331 );
+    ocl::test::assertEqual( ocl::full(r), 54.5981500331, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::log(a);
-    ocl::test::assertEqual( ocl::full(r), 1.38629436112 );
+    ocl::test::assertEqual( ocl::full(r), 1.38629436112, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::sqrt(a);
-    ocl::test::assertEqual( ocl::full(r), 2 );
+    ocl::test::assertEqual( ocl::full(r), 2, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::square(a);
-    ocl::test::assertEqual( ocl::full(r), 16 );
+    ocl::test::assertEqual( ocl::full(r), 16, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::sin(a);
-    ocl::test::assertEqual( ocl::full(r), -0.7568024953 );
+    ocl::test::assertEqual( ocl::full(r), -0.7568024953, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::cos(a);
-    ocl::test::assertEqual( ocl::full(r), -0.65364362086 );
+    ocl::test::assertEqual( ocl::full(r), -0.65364362086, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(4);
     auto r = ocl::tan(a);
-    ocl::test::assertEqual( ocl::full(r), 1.15782128235 );
+    ocl::test::assertEqual( ocl::full(r), 1.15782128235, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::asin(a);
-    ocl::test::assertEqual( ocl::full(r), 0.4555986733958234 );
+    ocl::test::assertEqual( ocl::full(r), 0.4555986733958234, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::acos(a);
-    ocl::test::assertEqual( ocl::full(r), 1.1151976533990733 );
+    ocl::test::assertEqual( ocl::full(r), 1.1151976533990733, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::atan(a);
-    ocl::test::assertEqual( ocl::full(r), 0.41450687458478597 );
+    ocl::test::assertEqual( ocl::full(r), 0.41450687458478597, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::abs(a);
-    ocl::test::assertEqual( ocl::full(r), 0.44 );
+    ocl::test::assertEqual( ocl::full(r), 0.44, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::inverse(a);
-    ocl::test::assertEqual( ocl::full(r), 2.272727272727273 );
+    ocl::test::assertEqual( ocl::full(r), 2.272727272727273, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::sinh(a);
-    ocl::test::assertEqual( ocl::full(r), 0.45433539871409734 );
+    ocl::test::assertEqual( ocl::full(r), 0.45433539871409734, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.44);
     auto r = ocl::cosh(a);
-    ocl::test::assertEqual( ocl::full(r), 1.0983718197972387 );
+    ocl::test::assertEqual( ocl::full(r), 1.0983718197972387, OCL_INFO);
   }
 
   // not supported
   // {
   //   auto a = ocl::Matrix(0.44);
   //   auto r = ocl::asinh(a);
-  //   ocl::test::assertEqual( ocl::full(r), 0.42691345412611653 );
+  //   ocl::test::assertEqual( ocl::full(r), 0.42691345412611653, OCL_INFO);
   // }
   // {
   //   auto a = ocl::Matrix(2.2);
   //   auto r = ocl::acosh(a);
-  //   ocl::test::assertEqual( ocl::full(r), 1.4254169430706127 );
+  //   ocl::test::assertEqual( ocl::full(r), 1.4254169430706127, OCL_INFO);
   // }
   // {
   //   auto a = ocl::Matrix(0.22);
   //   auto r = ocl::atanh(a);
-  //   ocl::test::assertEqual( ocl::full(r), 0.22365610902183242 );
+  //   ocl::test::assertEqual( ocl::full(r), 0.22365610902183242, OCL_INFO);
   // }
 
   // binary operations
@@ -121,48 +121,48 @@ TEST(Matrix, aScalarOperators)
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::plus(a,b);
-    ocl::test::assertEqual( ocl::full(r), 4.43 );
+    ocl::test::assertEqual( ocl::full(r), 4.43, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::minus(a,b);
-    ocl::test::assertEqual( ocl::full(r), -3.7699999999999996 );
+    ocl::test::assertEqual( ocl::full(r), -3.7699999999999996, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::ctimes(a,b);
-    ocl::test::assertEqual( ocl::full(r), 1.353 );
+    ocl::test::assertEqual( ocl::full(r), 1.353, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::cdivide(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.0804878048780488 );
+    ocl::test::assertEqual( ocl::full(r), 0.0804878048780488, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::cpow(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.010614686047848296 );
+    ocl::test::assertEqual( ocl::full(r), 0.010614686047848296, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::cmin(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.33 );
+    ocl::test::assertEqual( ocl::full(r), 0.33, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::cmax(a,b);
-    ocl::test::assertEqual( ocl::full(r), 4.1 );
+    ocl::test::assertEqual( ocl::full(r), 4.1, OCL_INFO);
   }
   {
     auto a = ocl::Matrix(0.33);
     auto b = ocl::Matrix(4.1);
     auto r = ocl::atan2(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.08031466966032468 );
+    ocl::test::assertEqual( ocl::full(r), 0.08031466966032468, OCL_INFO);
   }
 }
--- test/test_tensor.h
+++ test/test_tensor.h
@@ -18,9 +18,9 @@
 TEST(Tensor, aSlice) {
 
   auto a = ocl::Tensor::One(5,5)*10;
-  auto r = ocl::slice(a, {2}, ocl::end(a, 1) );
+  auto r = ocl::slice(a, {2}, ocl::end(a, 1));
 
-  ocl::test::assertEqual( ocl::full(r), 10 );
+  ocl::test::assertEqual( ocl::full(r), {{10}}, OCL_INFO);
 }
 
 TEST(Tensor, bScalarOperators) {
@@ -28,102 +28,102 @@ TEST(Tensor, bScalarOperators) {
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::uplus(a);
-    ocl::test::assertEqual( ocl::full(r), 4 );
+    ocl::test::assertEqual( ocl::full(r), {{4}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::uminus(a);
-    ocl::test::assertEqual( ocl::full(r), -4 );
+    ocl::test::assertEqual( ocl::full(r), {{-4}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::exp(a);
-    ocl::test::assertEqual( ocl::full(r), 54.5981500331 );
+    ocl::test::assertEqual( ocl::full(r), {{54.5981500331}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::log(a);
-    ocl::test::assertEqual( ocl::full(r), 1.38629436112 );
+    ocl::test::assertEqual( ocl::full(r), {{1.38629436112}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::sqrt(a);
-    ocl::test::assertEqual( ocl::full(r), 2 );
+    ocl::test::assertEqual( ocl::full(r), {{2}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::square(a);
-    ocl::test::assertEqual( ocl::full(r), 16 );
+    ocl::test::assertEqual( ocl::full(r), {{16}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::sin(a);
-    ocl::test::assertEqual( ocl::full(r), -0.7568024953 );
+    ocl::test::assertEqual( ocl::full(r), {{-0.7568024953}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::sin(a);
-    ocl::test::assertEqual( ocl::full(r), -0.7568024953 );
+    ocl::test::assertEqual( ocl::full(r), {{-0.7568024953}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::cos(a);
-    ocl::test::assertEqual( ocl::full(r), -0.65364362086 );
+    ocl::test::assertEqual( ocl::full(r), {{-0.65364362086}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(4);
     auto r = ocl::tan(a);
-    ocl::test::assertEqual( ocl::full(r), 1.15782128235 );
+    ocl::test::assertEqual( ocl::full(r), {{1.15782128235}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::asin(a);
-    ocl::test::assertEqual( ocl::full(r), 0.4555986733958234 );
+    ocl::test::assertEqual( ocl::full(r), {{0.4555986733958234}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::acos(a);
-    ocl::test::assertEqual( ocl::full(r), 1.1151976533990733 );
+    ocl::test::assertEqual( ocl::full(r), {{1.1151976533990733}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::atan(a);
-    ocl::test::assertEqual( ocl::full(r), 0.41450687458478597 );
+    ocl::test::assertEqual( ocl::full(r), {{0.41450687458478597}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::abs(a);
-    ocl::test::assertEqual( ocl::full(r), 0.44 );
+    ocl::test::assertEqual( ocl::full(r), {{0.44}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::inverse(a);
-    ocl::test::assertEqual( ocl::full(r), 2.272727272727273 );
+    ocl::test::assertEqual( ocl::full(r), {{2.272727272727273}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::sinh(a);
-    ocl::test::assertEqual( ocl::full(r), 0.45433539871409734 );
+    ocl::test::assertEqual( ocl::full(r), {{0.45433539871409734}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.44);
     auto r = ocl::cosh(a);
-    ocl::test::assertEqual( ocl::full(r), 1.0983718197972387 );
+    ocl::test::assertEqual( ocl::full(r), {{1.0983718197972387}}, OCL_INFO);
   }
   // {
   //   auto a = ocl::Tensor(0.44);
   //   auto r = ocl::asinh(a);
-  //   ocl::test::assertEqual( ocl::full(r), 0.42691345412611653 );
+  //   ocl::test::assertEqual( ocl::full(r), 0.42691345412611653, OCL_INFO);
   // }
   // {
   //   auto a = ocl::Tensor(2.2);
   //   auto r = ocl::acosh(a);
-  //   ocl::test::assertEqual( ocl::full(r), 1.4254169430706127 );
+  //   ocl::test::assertEqual( ocl::full(r), 1.4254169430706127, OCL_INFO);
   // }
   // {
   //   auto a = ocl::Tensor(0.22);
   //   auto r = ocl::atanh(a);
-  //   ocl::test::assertEqual( ocl::full(r), 0.22365610902183242 );
+  //   ocl::test::assertEqual( ocl::full(r), 0.22365610902183242, OCL_INFO);
   // }
 
   // binary operations
@@ -131,48 +131,48 @@ TEST(Tensor, bScalarOperators) {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::plus(a,b);
-    ocl::test::assertEqual( ocl::full(r), 4.43 );
+    ocl::test::assertEqual( ocl::full(r), {{4.43}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::minus(a,b);;
-    ocl::test::assertEqual( ocl::full(r), -3.7699999999999996 );
+    ocl::test::assertEqual( ocl::full(r), {{-3.7699999999999996}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::times(a,b);
-    ocl::test::assertEqual( ocl::full(r), 1.353 );
+    ocl::test::assertEqual( ocl::full(r), {{1.353}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::cdivide(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.0804878048780488 );
+    ocl::test::assertEqual( ocl::full(r), {{0.0804878048780488}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::cpow(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.010614686047848296 );
+    ocl::test::assertEqual( ocl::full(r), {{0.010614686047848296}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::cmin(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.33 );
+    ocl::test::assertEqual( ocl::full(r), {{0.33}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::cmax(a,b);
-    ocl::test::assertEqual( ocl::full(r), 4.1 );
+    ocl::test::assertEqual( ocl::full(r), {{4.1}}, OCL_INFO);
   }
   {
     auto a = ocl::Tensor(0.33);
     auto b = ocl::Tensor(4.1);
     auto r = ocl::atan2(a,b);
-    ocl::test::assertEqual( ocl::full(r), 0.08031466966032468 );
+    ocl::test::assertEqual( ocl::full(r), {{0.08031466966032468}}, OCL_INFO);
   }
 }
