5b12221a7797eded8dd63df2453c893b4b62497f
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-27 16:52:51 +0100

    casadi test braces

--- test/test_casadi.h
+++ test/test_casadi.h
@@ -20,6 +20,6 @@ TEST(Casadi, aScalarOperators)
   // scalar unary operations
   {
     ocl::CasadiMatrix m = ocl::CasadiMatrix(4.0);
-    ocl::test::assertEqual( ocl::casadi::full(m), {4.0} );
+    ocl::test::assertEqual( ocl::casadi::full(m), 4, OCL_INFO);
   }
 }
