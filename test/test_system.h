97a3b084ce77d631cfba221032472a0659381fb4
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-27 16:46:49 +0100

    system test, velocity

--- test/test_system.h
+++ test/test_system.h
@@ -23,16 +23,30 @@ TEST(System, aSystemEvaluation)
 {
   auto sys = ocl::System(&vars01Particle, &eq01Particle);
 
-  ocl::Matrix x = ocl::Matrix::Zero(2,1);
-  ocl::Matrix z = ocl::Matrix::Zero(0,1);
-  ocl::Matrix u = ocl::Matrix::Zero(1,1);
-  ocl::Matrix p = ocl::Matrix::Zero(0,1);
+  {
+    ocl::Matrix x = ocl::Matrix::Zero(2,1);
+    ocl::Matrix z = ocl::Matrix::Zero(0,1);
+    ocl::Matrix u = ocl::Matrix::Zero(1,1);
+    ocl::Matrix p = ocl::Matrix::Zero(0,1);
 
-  ocl::Matrix diff_out;
-  ocl::Matrix implicit_out;
-  sys.evaluate(x, z, u, p, diff_out, implicit_out);
+    ocl::Matrix diff_out;
+    ocl::Matrix implicit_out;
+    sys.evaluate(x, z, u, p, diff_out, implicit_out);
 
-  ocl::test::assertEqual( ocl::full(diff_out), {{0,-9.8}}, OCL_INFO);
+    ocl::test::assertEqual( ocl::full(diff_out), {{0,-9.8}}, OCL_INFO);
+  }
+  {
+    ocl::Matrix x = ocl::Matrix::One(2,1);
+    ocl::Matrix z = ocl::Matrix::Zero(0,1);
+    ocl::Matrix u = ctimes(ocl::Matrix::One(1,1), 4.0);
+    ocl::Matrix p = ocl::Matrix::Zero(0,1);
+
+    ocl::Matrix diff_out;
+    ocl::Matrix implicit_out;
+    sys.evaluate(x, z, u, p, diff_out, implicit_out);
+
+    ocl::test::assertEqual( ocl::full(diff_out), {{1,4-9.8}}, OCL_INFO);
+  }
 }
 
 void vars01Particle(ocl::SVH& sh)
